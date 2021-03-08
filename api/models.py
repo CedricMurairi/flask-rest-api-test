df806aec902fd0e9cd2aa0900aa79c4fb88ef37e
Cedric Murairi <murairicedric@gmail.com>
2021-03-08 18:30:24 +0200

    base model for api test --> User&Task

--- api/models.py
+++ api/models.py
@@ -1,11 +1,28 @@
 from flask_sqlalchemy import SQLAlchemy
 from . import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64))
     email = db.Column(db.String(64))
+    password_hash = db.Column(db.String(128))
+    # admin = db.Column(db.Boolean, default=False)
     tasks = db.relationship('Task', backref='user', lazy=True)
+
+    @property
+    def password(self):
+        raise AttributeError('Password is not readable')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password, password)
+    
+    def is_admin(self):
+        return self.admin
 
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
