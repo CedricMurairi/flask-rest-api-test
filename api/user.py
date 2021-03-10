443e371994cb7b93d1ce5a2f97c71a2fa8d4f8aa
Cedric Murairi <murairicedric@gmail.com>
2021-03-10 23:02:16 +0200

    final change to user resource -- end test | back to prod

--- api/user.py
+++ api/user.py
@@ -1,9 +1,23 @@
-from flask import Blueprint, request, abort
-from .models import User
+from flask import Blueprint, request, abort, make_response, current_app
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+from .models import User, token_required
 from . import db
 from .task import make_public
+from sqlalchemy import or_
 
 user = Blueprint('user', __name__)
+
+@user.errorhandler(404)
+def not_found(error):
+    return {'error': 'Not found'}, 404
+
+@user.errorhandler(400)
+def bad_request(error):
+    return {'error': 'Bad request'}, 400
+
+@user.errorhandler(500)
+def server_error(error):
+    return {'error': 'Internal server error'}, 500
 
 @user.route('/todo/api/v1.0/users', methods=['POST'])
 def register():
@@ -16,36 +30,64 @@ def register():
 
     user = User.query.filter_by(email=email).first()
     if user:
-       return {'error': 'User mail already exist'}, 400 
+       return {'error': 'User mail already exist'}, 400
 
-    new_use = User(name='Cedric', email=email, password=password)
+    new_use = User(name=username, email=email, password=password)
     db.session.add(new_use)
     db.session.commit()
 
     return {'message': 'User created successfully'}, 200
 
-# @user.route('/todo/api/v1.0/users', methods=['GET'])
-# def login():
-#     if not request.authorization:
-#         abort(400)
-
-#     email = request.authorization.username
-#     password = request.authorization.password
-#     user = User.query.filter(email=email)
-
-#     if user and user.verify_password(password):
-#         return {'message': 'Login successfull', 'data':
-#             {
-#                 'id': user.id,
-#                 'username': user.name,
-#                 'email': user.email,
-#                 'tasks': [make_public(taks) for taks in user.tasks]
-#             }
-#         }
-
-#     abort(400)
-
 @user.route('/todo/api/v1.0/users', methods=['GET'])
+def login():
+    if not request.authorization:
+        abort(400)
+
+    username = request.authorization.username
+    password = request.authorization.password
+    user = User.query.filter(or_(User.email==username, User.name==username)).first()
+
+    if user and user.verify_password(password):
+        s = Serializer(current_app.config['SECRET_KEY'])
+
+        token = user.generate_token()
+        print(token)
+        try:
+            data = s.loads(token)
+            print(data)
+        except Exception as e:
+            print("No data ", e)
+
+        return {'message': 'Login successfull', 'data':
+            {
+                'id': user.id,
+                'username': user.name,
+                'email': user.email,
+                'token': token,
+                'tasks': [make_public(taks) for taks in user.tasks]
+            }
+        }
+
+    abort(404)
+
+@user.route('/todo/api/v1.0/users/<int:id>', methods=['GET'])
+# @token_required
+def get_user(id):
+    user = User.query.get(id)
+
+    if not user:
+        abort(404)
+
+    return {'user':
+        {
+            'id': user.id,
+            'username': user.name,
+            'email': user.email,
+            'tasks': [make_public(taks) for taks in user.tasks]
+        }
+    }, 200
+
+@user.route('/todo/api/v1.0/users/all', methods=['GET'])
 # @admin_required
 def get_users():
     users = User.query.all()
@@ -57,10 +99,10 @@ def get_users():
             'email': user.email,
             'tasks': [make_public(taks) for taks in user.tasks]
         } for user in users
-    ]}
+    ]}, 200
 
 @user.route('/todo/api/v1.0/users/<int:id>', methods=['PUT'])
-# @login_required
+# @login_required and admin_can
 def update_user(id):
     user = User.query.get(id)
 
@@ -81,9 +123,9 @@ def update_user(id):
 
     return {'message': 'User updated succesfully'}
 
-@user.route('/todo/api/v1.0/users<int:id>', methods=['DELETE'])
+@user.route('/todo/api/v1.0/users/<int:id>', methods=['DELETE'])
 # @login_required
-# admin_required
+# @admin_required
 def delete_user(id):
     user = User.query.get(id)
 
@@ -92,5 +134,7 @@ def delete_user(id):
 
     db.session.delete(user)
     db.session.commit()
+
+    return {'message': 'User deleted succefully'}
 
     
